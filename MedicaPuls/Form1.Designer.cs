ce7d91e03944d0e162bd6b32a80eb5c5286d2cdd
Oleg <pop457045@mail.ru>
2017-09-07 15:57:54 +0300

    промежуточно

--- MedicaPuls/Form1.Designer.cs
+++ MedicaPuls/Form1.Designer.cs
@@ -28,20 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.bttn_open_file = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bttn_clr = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.splitContainer1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(805, 566);
+            this.panel1.TabIndex = 0;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.dataGridView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.bttn_clr);
+            this.splitContainer1.Panel2.Controls.Add(this.bttn_open_file);
+            this.splitContainer1.Size = new System.Drawing.Size(805, 566);
+            this.splitContainer1.SplitterDistance = 669;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // bttn_open_file
+            // 
+            this.bttn_open_file.Location = new System.Drawing.Point(13, 13);
+            this.bttn_open_file.Name = "bttn_open_file";
+            this.bttn_open_file.Size = new System.Drawing.Size(107, 23);
+            this.bttn_open_file.TabIndex = 0;
+            this.bttn_open_file.Text = "Open File";
+            this.bttn_open_file.UseVisualStyleBackColor = true;
+            this.bttn_open_file.Click += new System.EventHandler(this.bttn_open_file_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(666, 389);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // bttn_clr
+            // 
+            this.bttn_clr.Location = new System.Drawing.Point(13, 43);
+            this.bttn_clr.Name = "bttn_clr";
+            this.bttn_clr.Size = new System.Drawing.Size(107, 25);
+            this.bttn_clr.TabIndex = 1;
+            this.bttn_clr.Text = "Clear all";
+            this.bttn_clr.UseVisualStyleBackColor = true;
+            this.bttn_clr.Click += new System.EventHandler(this.bttn_clr_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(805, 566);
+            this.Controls.Add(this.panel1);
             this.Name = "MainForm";
             this.Text = "MedicaPuls";
+            this.panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button bttn_open_file;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button bttn_clr;
     }
 }
 
--- MedicaPuls/Form1.cs
+++ MedicaPuls/Form1.cs
@@ -15,6 +15,72 @@ namespace MedicaPuls
         public MainForm()
         {
             InitializeComponent();
+            this.dataGridView1.Columns.Add("first", "Давление");
+            this.dataGridView1.Columns.Add("secnd", "Пульс");
+            this.dataGridView1.Columns.Add("third", "Время");
+            this.dataGridView1.Columns.Add("fourth", "Поиск");
+        }
+
+        private void bttn_open_file_Click(object sender, EventArgs e)
+        {
+            
+            this.openFileDialog.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = openFileDialog.FileName;
+            // читаем файл в строку
+            string fileText = System.IO.File.ReadAllText(filename);
+            
+            string[] stroki = fileText.Split('\n');
+            int zapisei = stroki.Length;
+            int [,] cifrmass = new int[stroki.Length,4];
+            for (int i = 0; i< zapisei;i++)
+            {
+                if (stroki[i].Trim() != "")
+                {
+                    // adding to int array our mesuares (0-pressure, 1 - pulse , 2 - time , 3 - finder for start and end mes)
+                    cifrmass[i, 0] = Convert.ToInt32(stroki[i].Split(' ')[0]);
+                    cifrmass[i, 1] = Convert.ToInt32(stroki[i].Split(' ')[1]);
+                    cifrmass[i, 2] = Convert.ToInt32(stroki[i].Split(' ')[2]);
+                    cifrmass[i, 3] = cifrmass[i, 1]- cifrmass[i, 0];
+                }
+            }
+            int max_on_pulse = 0;
+            int min_on_pulse = 0;
+            int starting_pos = 0;
+            int ending_pos = 0;
+            for (int i = 0; i < zapisei; i++)
+            {
+                if (cifrmass[i, 1] > max_on_pulse) max_on_pulse = cifrmass[i, 1];
+                if (min_on_pulse > cifrmass[i, 3])
+                {
+                    min_on_pulse = cifrmass[i, 3];
+                }
+                if ((starting_pos == 0) && (min_on_pulse < cifrmass[i, 3]) && (cifrmass[i, 3] < 0)) starting_pos = i;
+            }
+
+
+
+            for (int i = (zapisei-2); i > 0; i--)
+            {
+                if ((cifrmass[i, 3] > cifrmass[i+1, 3]) && (cifrmass[i+1, 3]< 0))
+                {
+                    ending_pos = i-1;
+                    break;
+                }
+            }
+
+            for (int i = starting_pos; i < ending_pos; i++)
+            {
+                this.dataGridView1.Rows.Add(cifrmass[i, 0], cifrmass[i, 1], cifrmass[i, 2], cifrmass[i, 3]);
+            }
+        }
+
+        private void bttn_clr_Click(object sender, EventArgs e)
+        {
+            int coun = this.dataGridView1.Rows.Count;
+            for (int i = 0; i < coun; i++) this.dataGridView1.Rows.RemoveAt(0);
         }
     }
 }
